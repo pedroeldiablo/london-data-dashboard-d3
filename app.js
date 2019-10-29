15de1c556aa332227aa89b074d01c1a2259b5841
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-29 14:25:00 +0000

    exploring data sources due to coordinates issue with UK map data

--- app.js
+++ app.js
@@ -17,82 +17,87 @@
 // {"arcs":[[7,8,9,10,11]],"type":"Polygon","properties":{"NAME":"Berrylands","GSS_CODE":"E05000401","DISTRICT":"Kingston upon Thames","LAGSSCODE":"E09000021","HECTARES":145.39,"NONLD_AREA":0}},{"arcs":[[-6,12,-11,13,14,15]],"type":"Polygon"
 
 
+d3.queue()
+.defer(d3.json, './uk_topo_ward.json')
+// .defer(d3.json, './london_ward_lat_lng.json')
+  // .defer(d3.json, './LONDON_WARD.json')
+  .defer(d3.csv, './London-Data-Table-1.csv', function(row){
+    return {
+      country: row.country,
+      countryCode: row.countryCode,
+      population: +row.population,
+      medianAge: +row.medianAge,
+      fertilityRate: +row.fertilityRate,
+      populationDensity: +row.population / +row.landArea
+    };
+  })
+  .await(function(error, mapData, wardData){
+    if(error) throw error;
 
-// d3.queue()
-//   .defer(d3.json, '/LONDON_WARD.json')
-// //   .defer(d3.csv, './country_data.csv', function(row){
-// //     return {
-// //       country: row.country,
-// //       countryCode: row.countryCode,
-// //       population: +row.population,
-// //       medianAge: +row.medianAge,
-// //       fertilityRate: +row.fertilityRate,
-// //       populationDensity: +row.population / +row.landArea
-// //     };
-// //   })
-//   .await(function(error, mapData){
-//     if(error) throw error;
+    console.log(mapData);
 
-//     var geoData = topojson.feature(mapData, mapData.objects.London_Ward.geometries).features;
+    var geoData = topojson.feature(mapData, mapData.objects.wards).features;
 
-//     populationData.forEach(row => {
-//       var countries = geoData.filter(d => d.id === row.countryCode);
-//       countries.forEach(country => country.properties = row);
-//     });
-//     var width = 960;
-//     var height = 600;
+    wardData.forEach(row => {
+      var countries = geoData.filter(d => d.id === row.countryCode);
+      countries.forEach(country => country.properties = row);
+    });
+    var width = 600;
+    var height = 600;
 
-//     var projection = d3.geoMercator()
-//       .scale(125)
-//       .translate([width /2, height / 1.4]);
+    var projection = d3.geoMercator()
+      .center([0.2248, 51.5074])
+      .scale(50000)
+      .translate([width, height/2]);
 
-//     var path = d3.geoPath()
-//       .projection(projection);
+    var path = d3.geoPath()
+      .projection(projection);
 
-//     d3.select('svg')
-//       .attr('width', width)
-//       .attr('height', height)
-//       .selectAll('.country')
-//       .data(geoData)
-//       .enter()
-//       .append('path')
-//       .classed('country', true)
-//       .attr('d', path)
-//       .on('mousemove', showToolTip)
-//       .on('touchStart', showToolTip)
-//       .on('mouseout', hideToolTip)
-//       .on('touchEnd', hideToolTip);
+    d3.select('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('.country')
+      .data(geoData)
+      .enter()
+      .append('path')
+      .classed('country', true)
+      .attr('d', path)
+      .style('fill', 'transparent');
+    // .on('mousemove', showToolTip)
+    // .on('touchStart', showToolTip)
+    // .on('mouseout', hideToolTip)
+    // .on('touchEnd', hideToolTip);
 
-//     var select = d3.select('select');
+    //   var select = d3.select('select');
 
-//     select
-//       .on('change', d => setColor(d3.event.target.value));
+    //   select
+    //     .on('change', d => setColor(d3.event.target.value));
 
-//     setColor(select.property('value'));
+    //   setColor(select.property('value'));
 
 
-//     function setColor(val) {
-//       var colorRanges = {
-//         population: ['white', 'purple'],
-//         populationDensity: ['white', 'orange'],
-//         medianAge: ['deeppink', 'darkseagreen'],
-//         fertilityRate: ['pink', 'mediumseagreen']
-//       };
+    //   function setColor(val) {
+    //     var colorRanges = {
+    //       population: ['white', 'purple'],
+    //       populationDensity: ['white', 'orange'],
+    //       medianAge: ['deeppink', 'darkseagreen'],
+    //       fertilityRate: ['pink', 'mediumseagreen']
+    //     };
 
-//       var scale = d3.scaleLinear()
-//         .domain([0, d3.max(populationData, d => d[val])])
-//         .range(colorRanges[val]);
+    //     var scale = d3.scaleLinear()
+    //       .domain([0, d3.max(populationData, d => d[val])])
+    //       .range(colorRanges[val]);
 
-//       d3.selectAll('.country')
-//         .transition()
-//         .duration(750)
-//         .ease(d3.easeBackIn)
-//         .attr('fill', d => {
-//           var data = d.properties[val];
-//           return data ? scale(data) : '#ccc';
-//         });
-//     }
-//   });
+  //     d3.selectAll('.country')
+  //       .transition()
+  //       .duration(750)
+  //       .ease(d3.easeBackIn)
+  //       .attr('fill', d => {
+  //         var data = d.properties[val];
+  //         return data ? scale(data) : '#ccc';
+  //       });
+  //   }
+  });
 
 // var tooltip = d3.select('body')
 //   .append('div')
@@ -117,63 +122,3 @@
 //   tooltip
 //     .style('opacity', 0);
 // }
-
-d3.json('./london-topojson1.json', function(error, mapData) {
-  if (error) throw error;
-  console.log(mapData.objects.london_geo);
-  //   var path = d3.geoPath();
-  var width = 600;
-  var height = 600;
-
-  var geoData = topojson.feature(mapData, mapData.objects.london_geo).features;
-
-  //   var geoData = topojson.feature(mapData, mapData.objects.countries).features;
-
-
-  var projection = d3.geo.mercator()
-  .center([0.1248, 51.5074])
-    .scale(30000)
-    .translate([width / 1.5, height / 2]);
-
-
-  var path = d3.geo.path()
-    .projection(projection);
-  
-  d3.select('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .selectAll('path')
-    .data(geoData)
-    .enter()
-    .append('path')
-    .classed('country', true)
-    .style('fill', 'black')
-    .attr('d', path)
-        .on('mousemove', showToolTip)
-      .on('touchStart', showToolTip)
-      .on('mouseout', hideToolTip)
-      .on('touchEnd', hideToolTip);
-        
-});
-
-var tooltip = d3.select('body')
-  .append('div')
-  .classed('tooltip', true);
-
-function showToolTip(d) {
-    console.log(d);
-  var properties = d.properties;
-  tooltip
-    .style('opacity', 1)
-    .style('left', d3.event.x - (tooltip.node().offsetWidth /2) + 'px')
-    .style('top', d3.event.y + 25 + 'px')
-    .html(`
-        <p>${d.id}</p>
-      `);  
-}
-  
-function hideToolTip() {
-  tooltip
-    .style('opacity', 0);
-}
-
