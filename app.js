56f59b53254cca9d4556068358dd47fc26f51792
Peter Williams <pedroeldiablo@yahoo.com>
2019-11-01 00:57:15 +0000

    attempting to bring in transport data and show relative access and usage patterns

--- app.js
+++ app.js
@@ -85,11 +85,43 @@
 // Turnout at Mayoral election - 2012,
 
 
+// NLC,
+// TLC,
+// Station Name,
+// Region,
+// Local Authority,
+// Constituency,
+// OS Grid Easting,
+// OS Grid Northing,
+// Station Facility Owner,
+// Station Group,
+// PTE Urban Area Station,
+// London Travelcard Area,
+// SRS Code,
+// SRS Description,
+// NR Route,
+// CRP Line Designation,
+// Entries & Exits_Full,
+// Entries & Exits_Reduced,
+// Entries & Exits_Season,
+// 1718 Entries & Exits,
+// 1617 Entries & Exits,
+// 1718 Interchanges,
+// 1718 Entries & Exits - GB rank,
+// 1617 Entries & Exits - GB rank,
+// Large station change flag,
+// Small station change flag,
+// % Change,
+// Explanation of large change 1718,
+// Source for explanation of large change 1718
+
+
 d3.queue()
   .defer(d3.json, './uk_topo_ward.json')
   .defer(d3.json, './london_stations_topojson.json')
+  .defer(d3.json, './station-centroids.json')
   .defer(d3.csv, './London-Data-Table-1.csv', function(row){
-    console.log(row);
+    // console.log(row);
     var area = row['Area - Square Kilometres'];
     var population = parseFloat(row['Population - 2015'].replace(/,/g, ''));
     var openSpace = parseFloat(row['% area that is open space - 2014']);
@@ -114,27 +146,91 @@ d3.queue()
       hiddenDensity: hiddenDensity,
       jobs: jobs,
       jobsDensity: jobsDensity,
-      netEmployment:  jobs - workingAgePopulation,
+      netEmployment: jobs - workingAgePopulation,
       workingAge: workingAgePopulation,
       publicTransport: publicTransport,
       transportPopulationRating: publicTransport * population,
       transportDensityRating: publicTransport * trueDensity,
       transportJobs: jobs / publicTransport,
-      transportInvestmentImpact: publicTransport / (trueDensity * availableArea),
+      transportInvestmentImpact: publicTransport / (trueDensity * availableArea)
     };
   })
-  .await(function(error, mapData, stationsData, wardData){
+  .defer(d3.csv, './station-usage-2017-18-Travelcard.csv', function(row){
+    var name = row['Station Name'];
+    var entriesExits = parseFloat(row['1718 Entries & Exits'].replace(/,/g, '')) ? parseFloat(row['1718 Entries & Exits'].replace(/,/g, '')) : 0;
+    var interChanges = parseFloat(row['1718 Interchanges'].replace(/,/g, '')) ? parseFloat(row['1718 Interchanges'].replace(/,/g, '')) : 0;
+    var NLC = row['NLC'];
+    var route = row['NR Route'];
+    var srsCode = row['SRS Code'];
+    var routeDescription = row['SRS Description'];
+
+
+    return {
+      name: name,
+      entriesExits: entriesExits,
+      interChanges: interChanges,
+      allJournies: entriesExits +  interChanges,
+      NLC: NLC,
+      route: route,
+      srsCode: srsCode,
+      routeDescription: routeDescription
+    };
+  })
+  .await(function(error, mapData, stationsData, allStationsData, wardData, stationUsageData){
     if(error) throw error;
 
     // console.log(mapData);
 
     var geoData = topojson.feature(mapData, mapData.objects.wards).features;
     var stationsGeoData = topojson.feature(stationsData, stationsData.objects.london_stations).features;
+    //  var allStationsGeoData = topojson.feature(stationsData, stationsData.objects.london_stations).features;
+    var allStationsGeoData = allStationsData.features;
+
+    //  console.log('stationsGeoData', stationsGeoData);
+
+    console.log('allStationsGeoData', allStationsGeoData);
+      
+
 
     wardData.forEach(row => {
       var countries = geoData.filter(d => d.id === row.newCode);
-      countries.forEach(country => country.properties = row);
+      countries.forEach(country => {
+        console.log('country before', country);
+        country.properties = row;
+        console.log('country after', country);
+      
+      });
     });
+
+    stationUsageData.forEach(row => {
+      console.log('row', row);
+      
+      var stations = allStationsGeoData.filter(d => {
+        console.log('data d', d);
+        return d.properties.nlc_id === +row.NLC;
+      }
+        
+      );
+      console.log('stations', stations);
+      stations.forEach(station => {
+        console.log('station before', station);
+        station.properties = row;
+        console.log('station after', station);
+      });
+    });
+
+    // allStationsGeoData.forEach(row => {
+      
+    //   var stations = stationUsageData.filter(d => 
+    //     +d.NLC === row.properties.nlc_id
+    //   );
+
+    //   stations.forEach(station => {
+    //     console.log('station before', station);
+    //     station.properties = row;
+    //     console.log('station after', station);
+    //   });
+    // });
 
     var trainlines = [];
   
@@ -148,22 +244,22 @@ d3.queue()
         // console.log(line);
         var lineName = line.name; 
         if(trainlines[`${lineName}`]){
-          trainlines[`${lineName}`].push({"geometry":{"coordinates": coordinates, type: "MultiPolygon"}, type: "Feature"});
+          trainlines[`${lineName}`].push({'geometry': {'coordinates': coordinates, type: 'MultiPolygon'}, type: 'Feature'});
 
         } else {
           trainlines[`${lineName}`] = [];
-          trainlines[`${lineName}`].push({"geometry":{"coordinates": coordinates, type: "MultiPolygon"}, type: "Feature"});
+          trainlines[`${lineName}`].push({'geometry': {'coordinates': coordinates, type: 'MultiPolygon'}, type: 'Feature'});
         }
       });
     });
 
-    console.log('stationsGeoData', stationsGeoData);
-    console.log('trainlines', trainlines);
+    // console.log('stationsGeoData', stationsGeoData);
+    // console.log('trainlines', trainlines);
 
-    console.log( trainlines);
-    console.log("hi");
+    // console.log( trainlines);
+    // console.log("hi");
 
-    console.log('length', trainlines, Object.keys(trainlines), trainlines.length);
+    // console.log('length', trainlines, Object.keys(trainlines), trainlines.length);
 
     var lineNames = Object.keys(trainlines);
 
@@ -185,15 +281,11 @@ d3.queue()
         .on('touchStart', showToolTip)
         .on('mouseout', hideToolTip)
         .on('touchEnd', hideToolTip);
-        console.log(trainlines[line]);
+      // console.log(trainlines[line]);
    
     }
     );
       
-    console.log("hi");
-
-   
-
     // console.log('stationsGeoData', stationsGeoData);
     // console.log('linesGeoData', linesGeoData);
 
@@ -206,12 +298,18 @@ d3.queue()
       .scale(45000)
       .translate([width, height/2]);
 
+    var projection2 = d3.geoMercator()
+      .center([0.3848, 51.5074])
+      .scale(45000)
+      .translate([width, height/2]);
+
     var path = d3.geoPath()
       .projection(projection);
     
-    var path2 = d3.geoPath();
+    var path2 = d3.geoPath()
+      .projection(projection2);
 
-    console.log("geoData", geoData);
+    // console.log("geoData", geoData);
 
     d3.select('#map')
       .attr('width', width)
@@ -221,27 +319,76 @@ d3.queue()
       .enter()
       .append('path')
       .classed('country', true)
-      .attr('d', path)
-      // .style('fill', 'transparent')
+      .attr('d', path);
+    // .on('mousemove', showToolTip)
+    // .on('touchStart', showToolTip)
+    // .on('mouseout', hideToolTip)
+    // .on('touchEnd', hideToolTip);
+
+    d3.select('#map')
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('.station')
+      .data(allStationsGeoData)
+      .enter()
+      .append('path')
+      .classed('station', true)
+      .attr('d', path2)
+      .style('fill', 'lavenderblush')
+      .attr('r', '50')
       .on('mousemove', showToolTip)
       .on('touchStart', showToolTip)
       .on('mouseout', hideToolTip)
       .on('touchEnd', hideToolTip);
 
-    // d3.select('#map')
-    //   .attr('width', width)
-    //   .attr('height', height)
-    //   .selectAll('.station')
-    //   .data(stationsGeoData)
-    //   .enter()
-    //   .append('path')
-    //   .classed('station', true)
-    //   .attr('d', path)
-    //   .style('fill', 'red')
-    //   .on('mousemove', showToolTip)
-    //   .on('touchStart', showToolTip)
-    //   .on('mouseout', hideToolTip)
-    //   .on('touchEnd', hideToolTip);
+    d3.select('#map')
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('.tubestation')
+      .data(stationsGeoData)
+      .enter()
+      .append('path')
+      .classed('tubestation', true)
+      .attr('d', path)
+      // .style('fill', 'blue')
+      .on('mousemove', showToolTip)
+      .on('touchStart', showToolTip)
+      .on('mouseout', hideToolTip)
+      .on('touchEnd', hideToolTip);
+
+    d3.selectAll('.tubestation')
+      .transition()
+      .duration(750)
+      .ease(d3.easeBackIn)
+      .attr('fill', d => {
+        var color;
+        var lineColors = {
+          'Bakerloo': '#B36305', 	
+          'Central': '#E32017',	
+          'Circle': '#FFD300',
+          'District': '#00782A',	
+          'Hammersmith & City': '#F3A9BB',	
+          'Jubilee': '#A0A5A9',	
+          'Metropolitan': '#9B0056',	
+          'Northern': '#000000',
+          'Piccadilly': '#003688',	
+          'Victoria': '#0098D4',	
+          'Waterloo & City': '#95CDBA',
+          'DLR': '#00A4A7',	
+          'London Overground': '#EE7C0E',
+          'Tramlink': '#84B817',	
+          'Emirates Air Line': '#E21836',	
+          'Crossrail': '#7156A5',
+          'TfL Rail': '#0019a8',
+          'East London': 'orange'
+        };
+        var tubeLines = d.properties.lines;
+        tubeLines.forEach(line => {
+        // console.log('line', line.name, lineColors[line.name]);
+          return color = lineColors[line.name];
+        });
+        return color;
+      });
 
     var select = d3.select('select');
 
@@ -262,16 +409,16 @@ d3.queue()
         publicTransport: ['pink', 'mediumseagreen'], 
         transportPopulationRating: ['pink', 'mediumseagreen'], 
         transportDensityRating: ['pink', 'mediumseagreen'],
-        transportJobs:  ['pink', 'mediumseagreen'], 
+        transportJobs: ['pink', 'mediumseagreen'], 
         transportInvestmentImpact: ['pink', 'mediumseagreen'],
         jobsDensity: ['pink', 'mediumseagreen'],
-        hiddenDensity: ['pink', 'mediumseagreen'],
+        hiddenDensity: ['pink', 'mediumseagreen']
       };
 
       var scale = d3.scaleLinear()
         .domain([d3.min(wardData, d => d[val]), d3.max(wardData, d => d[val])])
         .range(colorRanges[val]);
-
+      
       d3.selectAll('.country')
         .transition()
         .duration(750)
@@ -280,15 +427,77 @@ d3.queue()
           var data = d.properties[val];
           return data ? scale(data) : '#ccc';
         });
+
+          
+    
+
+      // console.log(lineColors.Bakerloo);
+
+     
+
+   
     }
-  });
+});
+
+//   Bakerloo: (25) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Central: (49) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Circle: (36) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Crossrail: (41) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// DLR: (45) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// District: (60) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// East London: (9) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Emirates Air Line: (2) [{…}, {…}]
+// Hammersmith & City: (29) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Jubilee: (27) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// London Overground: (112) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, …]
+// Metropolitan: (38) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Northern: (52) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Piccadilly: (52) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Piccadily: [{…}]
+// TfL Rail: (14) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Tramlink: (39) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Victoria: (16) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// Waterloo & City
 
 var tooltip = d3.select('body')
   .append('div')
   .classed('tooltip', true);
 
+// function showToolTip(d) {
+//   console.log(d);
+//   var properties = d.properties;
+//   tooltip
+//     .style('opacity', 1)
+//     // .style('left', d3.event.x - (tooltip.node().offsetWidth /2) + 'px')
+//     // .style('top', d3.event.y + 25 + 'px')
+
+//     .html(`
+//         <p>${properties.wardName}</p>
+//         <p>Population: ${properties.population}</p>
+//         <p>Area: ${properties.area} km2</p>
+//         <p>Open Space: ${properties.openSpace}%</p>
+//         <p>density: ${properties.density}</p>
+//         <p>hiddenDensity: ${properties.hiddenDensity}</p>
+//         <p>openSpace: ${properties.openSpace}</p>
+//         <p>availableArea: ${properties.availableArea} km2</p>
+//         <p>trueDensity: ${properties.trueDensity} per/km2</p>
+//         <p>jobs: ${properties.jobs}</p>
+//          <p>jobsDensity: ${properties.jobsDensity}</p>
+//         <p>Working age: ${properties.workingAge}</p>
+//         <p>Net Employment: ${properties.netEmployment}</p>
+//         <p>publicTransport: ${properties.publicTransport}</p>
+//         <p>transportJobs: ${properties.transportJobs}</p>
+//         <p>transportInvestmentImpact: ${properties.transportInvestmentImpact}</p>
+//       `);  
+// }
+  
+// function hideToolTip() {
+//   tooltip
+//     .style('opacity', 0);
+// }
+
 function showToolTip(d) {
-   console.log(d);
+  console.log(d);
   var properties = d.properties;
   tooltip
     .style('opacity', 1)
@@ -296,22 +505,14 @@ function showToolTip(d) {
     // .style('top', d3.event.y + 25 + 'px')
 
     .html(`
-        <p>${properties.wardName}</p>
-        <p>Population: ${properties.population}</p>
-        <p>Area: ${properties.area} km2</p>
-        <p>Open Space: ${properties.openSpace}%</p>
-        <p>density: ${properties.density}</p>
-        <p>hiddenDensity: ${properties.hiddenDensity}</p>
-        <p>openSpace: ${properties.openSpace}</p>
-        <p>availableArea: ${properties.availableArea} km2</p>
-        <p>trueDensity: ${properties.trueDensity} per/km2</p>
-        <p>jobs: ${properties.jobs}</p>
-         <p>jobsDensity: ${properties.jobsDensity}</p>
-        <p>Working age: ${properties.workingAge}</p>
-        <p>Net Employment: ${properties.netEmployment}</p>
-        <p>publicTransport: ${properties.publicTransport}</p>
-        <p>transportJobs: ${properties.transportJobs}</p>
-        <p>transportInvestmentImpact: ${properties.transportInvestmentImpact}</p>
+    <p>Name: ${properties.name}</p>
+    <p>entriesExits: ${properties.entriesExits}</p>
+    <p>interChanges: ${properties.interChanges}</p>
+    <p>allJournies: ${properties. allJournies}</p>
+    <p>NLC: ${properties.NLC}</p>
+    <p>route: ${properties.route}</p>
+    <p>srsCode: ${properties.srsCode}</p>
+    <p>routeDescription: ${properties.routeDescription}</p>
       `);  
 }
   
---
+++ tubecolors.js
@@ -0,0 +1,17 @@
+
+{"Bakerloo": "#B36305", 	
+"Central": "#E32017",	
+"Circle": "#FFD300",
+"District": "#00782A",	
+"Hammersmith and City": "#F3A9BB",	
+"Jubilee": "#A0A5A9",	
+"Metropolitan": "#9B0056",	
+"Northern": "#000000",
+"Piccadilly": "#003688",	
+"Victoria": "#0098D4",	
+"Waterloo and City": "#95CDBA",
+"DLR": "#00A4A7",	
+"Overground": "#EE7C0E",
+"Tramlink": "#84B817",	
+"Cable Car": "#E21836",	
+"Crossrail": "#7156A5"}
